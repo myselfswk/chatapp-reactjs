d2e402924100c88a34b82b1c253f79fce8de9d74
Muhammad Waleed Khan <fastnegative1004@gmail.com>
2021-07-27 17:59:44 +0500

    add componenets
    add chat app all components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
+import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID="93c8acf6-8526-453b-96de-d96dfeac52eb"
+            userName="myselfswk"
+            userSecret="waleed1999"
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+        />
+    )
 }
 
 export default App;
---
+++ src/components/TheirMessages.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+const TheirMessages = ({ message, lastMessage }) => {
+    const isFirstMessageByUser = !lastMessage || lastMessage.sender.username !== message.sender.username;
+
+    return (
+        <div className="message-row">
+            {
+                isFirstMessageByUser && (
+                    <div
+                        className="message-avatar"
+                        style={{
+                            backgroundImage: message.sender && `url(${message.sender.avatar})`
+                        }}
+                    />
+                )
+            }
+
+            {
+                message.attachments && message.attachments.length > 0
+                    ? (
+                        <img
+                            src={message.attachments[0].file}
+                            alt="messgae-attachments"
+                            className="message-image"
+                            style={{ marginLeft: isFirstMessageByUser ? '4px' : '48px' }}
+                        />
+                    )
+                    : (
+                        <div
+                            className="message"
+                            style={{
+                                float: 'left',
+                                backgroundColor: '#CABCDC',
+                                marginLeft: isFirstMessageByUser ? '4px' : '48px'
+                            }}
+                        >
+                            {message.text}
+                        </div>
+                    )
+            }
+        </div>
+    )
+}
+
+export default TheirMessages;
