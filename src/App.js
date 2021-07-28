8a2382a3a6d0e1c4957a54828460a5c7ba8ad40c
Muhammad Waleed Khan <fastnegative1004@gmail.com>
2021-07-28 11:50:55 +0500

    update files
    working on design and some logics

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import { ChatEngine } from 'react-chat-engine';
 import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 
 import './App.css';
 
 const App = () => {
+
+    if (!localStorage.getItem('username')) {
+        return <LoginForm />
+    }
+
+    const projectID = "93c8acf6-8526-453b-96de-d96dfeac52eb";
     return (
         <ChatEngine
             height="100vh"
-            projectID="93c8acf6-8526-453b-96de-d96dfeac52eb"
-            userName="myselfswk"
-            userSecret="waleed1999"
+            projectID={projectID}
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
             renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+            onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
         />
     )
 }
